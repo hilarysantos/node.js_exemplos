415f4e73674e29afc0829515a0f8fdb45f2866ab
Hilary Santos <104283876+hilarysantos@users.noreply.github.com>
2022-08-09 15:17:30 -0300

    Códigos da aula 02

--- index.js
+++ index.js
@@ -29,3 +29,41 @@ const server = http.createServer(function (req, res) {
 });
 server.listen(3000);
 console.log('Servidor funcionando. Acesse: localhost:3000');
+
+//Aula 02: Trabalhando com métodos HTTP e rotas
+
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    if(req.method === 'GET'){
+        if (req.url === '/'){
+            res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+            res.end('<html><body><h1>Respondendo uma requisição GET!</h1></body></html>')
+        }else if(req.url === '/fotos'){
+            res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+            res.end('<html><body><h1>Cheguei na rota de fotos!</h1></body></html>')
+        }
+        else if(req.url === '/gatos'){
+            res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+            res.end('<html><body><h1>Cheguei na rota de fotos de gatos!</h1></body></html>')
+        }
+        else {
+            res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+            res.end('<html><body><h1>Não encontrei uma rota!</h1></body></html>')
+        }
+    }else if(req.method === 'POST'){
+        res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+        res.end('Repondendo uma requisição POST!')
+    }else if(req.method === 'PUT'){
+        res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+        res.end('Repondendo uma requisição PUT!')
+    }else if(req.method === 'DELETE'){
+        res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+        res.end('Repondendo uma requisição DELETE!')
+    }else{
+        res.writeHead(400, {'Content-type': 'text/html; charset=utf-8'})
+        res.end('Não sei tratar esse método HTTP: ${re.method}')
+    }
+})
+server.listen(3000)
+console.log('Servidor funcionando. Acesse: localhost:3000');

20f1084eff34b2c103eae1432ce8a5993ebf9a0d
Hilary Santos <104283876+hilarysantos@users.noreply.github.com>
2022-08-09 14:41:36 -0300

    Códigos extras da aula 01

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Aula 01
+
+//Primeiro exemplo: Imprimindo texto no terminal (PowerShell)
 console.log("Olá, Toti!")
 
+//Segundo Exemplo: Primeira criação de servidor
 const http = require('http');
 const server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
@@ -8,3 +11,21 @@ const server = http.createServer(function (req, res) {
 });
 server.listen(3000);
 console.log('Servidor funcionando com sucesso! Acesse: localhost:3000');
+
+//Terceiro Exemplo: Apresentando o trabalho com URLs
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-type': 'text/html; charset=utf-8'});
+    if(req.url == "/"){
+        res.write("<h1> Olá, Toti! </h1>");
+    }else if(req.url == "/outra/"){
+        res.write("<h1> Outra Página </h1>");
+    }else{
+        res.writeHead(404, {'Content-type': 'text/html; charset=utf-8'});
+        res.write("<h1> Página não encontrada! </h1>");
+    }
+    res.end();
+});
+server.listen(3000);
+console.log('Servidor funcionando. Acesse: localhost:3000');
